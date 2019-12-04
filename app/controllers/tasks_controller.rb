4fb87707acb4b8130677f2c2c6c96532adc875bc
taro.kirameki <taro.kirameki@techacademy.jp>
2019-12-04 04:56:43 +0000

    kadai-submit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,17 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  
   def index
-    @tasks = Task.all.page(params[:page])
+    @tasks = current_user.tasks.build  # form_with 用
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+     unless logged_in?
+      redirect_to login_url
+     end
   end
 
+      
+      
   def show
   end
 
@@ -12,7 +20,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が追加されました'
